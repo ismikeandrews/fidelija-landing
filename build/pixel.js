b926a94cf55ae010f5c60919baa2c4a748c525a0
Michael Andrews <michaelandrews@Michaels-MacBook-Pro.local>
2022-01-24 17:49:36 -0300

    Updating event code

--- build/pixel.js
+++ build/pixel.js
@@ -6,5 +6,5 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '3128191200832106');
+fbq('init', '1001194587481667');
 fbq('track', 'PageView');
