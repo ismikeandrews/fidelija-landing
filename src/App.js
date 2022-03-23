ced441412dafdf8e872857cdad4ab7b6828fb6aa
Michael Andrews <michaelandrews@Michaels-MacBook-Pro.local>
2022-03-23 20:06:27 -0300

    Removing analitics tags from header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Routes from './Routes'
 import './global.css';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-Y020XVNYRS"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
     return (
